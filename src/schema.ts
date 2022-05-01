a77a364e5e4b05987717bf23b8226299890a527c
kuhuisien <>
2022-05-01 11:48:49 +0800

    add query schema

--- src/schema.ts
+++ src/schema.ts
@@ -12,10 +12,34 @@ export const typeDefs = gql`
     author: String
   }
 
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    createdAt: String!
+    published: Boolean!
+    user: User!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    profile: Profile!
+    posts: [Post!]!
+  }
+
+  type Profile {
+    id: ID!
+    bio: String!
+    user: User!
+  }
+
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
+    user: User
   }
 `;
