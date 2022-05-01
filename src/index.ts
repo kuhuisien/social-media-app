5ee523b9ccc200975c08efcda2ceb26bf73e576c
kuhuisien <>
2022-05-01 13:21:05 +0800

    implement post creation with prisma

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
 import { ApolloServer } from "apollo-server";
-import { Query } from "./resolvers";
+import { Mutation, Query } from "./resolvers";
 import { typeDefs } from "./schema";
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export interface IContext {
+  prisma: PrismaClient<
+    Prisma.PrismaClientOptions,
+    never,
+    Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+  >;
+}
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array.
 const resolvers = {
   Query,
+  Mutation,
 };
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, context: { prisma } });
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
--- src/schema.ts
+++ src/schema.ts
@@ -35,11 +35,24 @@ export const typeDefs = gql`
     user: User!
   }
 
+  type UserError {
+    message: String!
+  }
+
+  type PostPayload {
+    userErrors: [UserError!]!
+    post: Post
+  }
+
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
     user: User
+  }
+
+  type Mutation {
+    postCreate(title: String!, content: String!): PostPayload
   }
 `;
