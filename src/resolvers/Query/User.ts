2d37dd73415210befc8b2855a5f7e097e9029faa
kuhuisien <>
2022-05-03 17:31:38 +0800

    add comments

--- src/resolvers/Query/User.ts
+++ src/resolvers/Query/User.ts
@@ -8,12 +8,16 @@ export const User = {
   posts: ({ id }: IUserParentType, __: any, { prisma, userInfo }: IContext) => {
     const isOwnProfile = id === userInfo?.userId;
 
+    // returns all posts belong to id, regardless it is published or not
+    // (User can view his own posts, no matter it is published or not)
     if (isOwnProfile) {
       return prisma.post.findMany({
         where: { authorId: id },
         orderBy: [{ createdAt: "desc" }],
       });
-    } else {
+    }
+    // returns all posts belong to id, which is published
+    else {
       return prisma.post.findMany({
         where: { authorId: id, published: true },
         orderBy: [{ createdAt: "desc" }],
